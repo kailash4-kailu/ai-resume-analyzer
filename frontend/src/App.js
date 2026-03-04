6c042919610da47b33f62e74e6cfd763a0dfc59d
Somisetty Jagannada Kailash <somisettyvasantharani@gmail.com>
2026-03-04 10:23:05 +0530

    fix backend api url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
     try {
 
       const response = await axios.post(
-        "http://127.0.0.1:8000/analyze-resume/",
+        "https://ai-resume-analyzer-roal.onrender.com/analyze-resume/",
         formData
       );
 
